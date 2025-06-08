0f00b4def18dc62e876fae60507d38a5350f65ed
Meloncaully <maloneingham@gmail.com>
2025-06-08 15:42:29 -0400

    implemented Solver

--- src/cells.py
+++ src/cells.py
@@ -7,6 +7,7 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self.__x1 = -1
         self.__x2 = -1
         self.__y1 = -1
@@ -16,22 +17,36 @@ class Cell:
     def draw(self, x1, y1, x2, y2):
         if self.__win is None:
             return
+        
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
         self.__y2 = y2
+
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(line, "white")
         
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self.__x2 - self.__x1) // 2
--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
 from graphics import Window
 from maze import Maze
-import unittest
+import sys
 
 def main():
-    num_rows = 20
-    num_cols = 30
+    num_rows = 12
+    num_cols = 13
     margin = 50 
     screen_x = 800
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("Maze Created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("Maze can not be solved")
+    else:
+        print("Maze Solved!")
 
     win.wait_for_close()
 
